01c26d09690b8299e5249b308cb7faf8f76fbac5
GianmariaFatta <gianmaria.fatta@gmail.com>
2023-02-22 17:05:10 +0100

    add hello

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $hello = 'Helloworld';
+    return view('home', compact ('hello'));
 });
+
